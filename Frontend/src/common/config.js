7668be0ec1d8f16da6594668b40b201039db8af1
Ashish <ashisprusty71@gmail.com>
2025-09-05 00:04:27 +0530

    fix: prod issue fixed2

--- Frontend/src/common/config.js
+++ Frontend/src/common/config.js
@@ -2,5 +2,5 @@ const env = "prod";
 
 export const API_BASE_URL =
   env === "dev"
-    ? "http://localhost:5000/api"
+    ? "https://bharati-sweets-backend.onrender.com/api"
     : "https://bharati-sweets-backend.onrender.com/api";
