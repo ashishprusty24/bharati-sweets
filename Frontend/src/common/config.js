59f84545e33ca86810647af6504b89de25b78f73
Ashish <ashisprusty71@gmail.com>
2025-09-08 22:03:33 +0530

    fix : frontend final code added

--- Frontend/src/common/config.js
+++ Frontend/src/common/config.js
@@ -1,6 +1,6 @@
-const env = "dev";
+const env = "prod";
 
 export const API_BASE_URL =
   env === "dev"
     ? "http://localhost:5000/api"
-    : "https://bharati-sweets-backend.onrender.com/api";
+    : "https://bharati-sweets-prod.onrender.com/api";
