7fafba6fe3e71c627d8c868ce27b2cbaffbe2a83
Ashish <ashisprusty71@gmail.com>
2025-09-10 23:18:17 +0530

    fix : dev url isue fixed

--- Frontend/src/common/config.js
+++ Frontend/src/common/config.js
@@ -2,7 +2,7 @@ const env = "dev";
 
 export const API_BASE_URL =
   env === "dev"
-    ? "https://bharati-sweets-backend.onrender.com"
+    ? "https://bharati-sweets-backend.onrender.com/api"
     : "https://bharati-sweets-prod.onrender.com/api";
 
 // https://bharati-sweets-backend.onrender.com
